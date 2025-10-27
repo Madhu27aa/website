08ac9f1baa408871fd4b472fbde3f7fcf9c0f4aa
Madhumitha Seshaiah <152010063+Madhu27aa@users.noreply.github.com>
2025-10-27 14:25:54 -0500

    Add new tech stack icons for various technologies

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -16,40 +16,76 @@ import {
   SiNextdotjs,
   SiSolidity,
   SiPostgresql,
+  SiKotlin,
+  SiSwift,
+  SiHtml5,
+  SiCss3,
+  SiAngular,
+  SiVuedotjs,
+  SiDotnet,
+  SiSpring,
+  SiDjango,
+  SiFlask,
+  SiFigma,
+  SiAws,
+  SiAzure,
+  SiGooglecloud,
+  SiHadoop,
+  SiApachekafka,
+  SiDocker,
+  SiTensorflow,
+  SiKeras,
+  SiPytorch,
+  SiOpencv,
+  SiNltk,
 } from "react-icons/si";
 import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
-       <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-       <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
+      {/* Programming Languages */}
+      <Col xs={4} md={2} className="tech-icons"><DiJava /></Col>
+      <Col xs={4} md={2} className="tech-icons"><DiPython /></Col>
+      <Col xs={4} md={2} className="tech-icons"><CgCPlusPlus /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiSwift /></Col>
+      <Col xs={4} md={2} className="tech-icons"><TbBrandGolang /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiKotlin /></Col>
 
-     
+      {/* Web Technologies */}
+      <Col xs={4} md={2} className="tech-icons"><SiHtml5 /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiCss3 /></Col>
+      <Col xs={4} md={2} className="tech-icons"><DiJavascript1 /></Col>
+      <Col xs={4} md={2} className="tech-icons"><DiReact /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiAngular /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiVuedotjs /></Col>
+      <Col xs={4} md={2} className="tech-icons"><DiNodejs /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiDotnet /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiSpring /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiDjango /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiFlask /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiFigma /></Col>
+
+      {/* Tools & Cloud */}
+      <Col xs={4} md={2} className="tech-icons"><SiAws /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiAzure /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiGooglecloud /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiHadoop /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiApachekafka /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiDocker /></Col>
+      <Col xs={4} md={2} className="tech-icons"><DiMongodb /></Col>
+      <Col xs={4} md={2} className="tech-icons"><DiGit /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiFirebase /></Col>
+
+      {/* AI/ML */}
+      <Col xs={4} md={2} className="tech-icons"><SiTensorflow /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiKeras /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiPytorch /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiOpencv /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiNltk /></Col>
     </Row>
   );
 }
 
 export default Techstack;
+
